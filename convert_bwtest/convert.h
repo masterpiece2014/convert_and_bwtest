2bd184739114e40e5b1d3c68063e7c558bea4845
cbw <cbw@cbw.(none)>
2013-08-02 02:29:11 +0800

    changed new way of converting floats to str; new range check cannot work

--- convert_bwtest/convert.h
+++ convert_bwtest/convert.h
@@ -32,25 +32,117 @@
 
 //#include "StarterCfgDef.h"
 
-
-#include <sstream>
-#include <numeric>
-#include <limits>
-#include <stdexcept>
+#include <cstddef>
+#include <cstdint>
 #include <type_traits>
+#include <limits>
+
 #include <string>
 #include <cstdlib>
 #include <cstring>
+#include <cstdio>
+#include <cstdarg>
+
+#include <stdexcept>
 
 
-//__NAMESPACE_STARTER___BEGIN__
+#ifdef __GNUC__
+#define likely(x)   (__builtin_expect((x), 1))
+#define unlikely(x) (__builtin_expect((x), 0))
+#else
+#define likely(x)   (x)
+#define unlikely(x) (x)
+#endif
 
-#undef RANGE_CHEACK
-#define RANGE_CHEACK(val, TypeName)\
+namespace detail_ {
+
+    template <typename LHS, LHS lhs, typename RHS>
+    inline bool greater_than_impl(
+        typename std::enable_if <
+        (lhs <= std::numeric_limits<RHS>::max()
+        && lhs >= std::numeric_limits<RHS>::min()),
+        RHS
+        >::type const rhs
+    ) {
+        return lhs < rhs;
+    }
+
+    template <typename LHS, LHS lhs, typename RHS>
+    inline bool greater_than_impl(
+        typename std::enable_if <
+        (lhs > std::numeric_limits<RHS>::max()),
+        RHS
+        >::type const
+    ) {
+        return false;
+    }
+
+    template <typename LHS, LHS lhs, typename RHS>
+    inline bool greater_than_impl(
+        typename std::enable_if <
+        (lhs < std::numeric_limits<RHS>::min()),
+        RHS
+        >::type const
+    )   {
+        return true;
+    }
+
+    template <typename RHS, RHS rhs, typename LHS>
+    inline bool less_than_impl(
+        typename std::enable_if <
+        (rhs > std::numeric_limits<LHS>::max()),
+        LHS
+        >::type const
+    ) {
+        return true;
+    }
+
+    template <typename RHS, RHS rhs, typename LHS>
+    inline bool less_than_impl(
+        typename std::enable_if <
+        (rhs < std::numeric_limits<LHS>::min()),
+        LHS
+        >::type const
+    ) {
+        return false;
+    }
+
+} // detail_
+
+template <typename LHS, LHS lhs, typename RHS>
+inline bool greater_than(RHS const rhs) {
+    return detail_::greater_than_impl <
+           LHS, lhs, typename std::remove_reference<RHS>::type
+           > (rhs);
+}
+template <typename RHS, RHS rhs, typename LHS>
+inline bool less_than(LHS const lhs) {
+    return detail_::less_than_impl <
+           RHS, rhs, typename std::remove_reference<LHS>::type
+           > (lhs);
+}
+
+//#undef CONV_RANGE_CHEACK
+//#define CONV_RANGE_CHEACK(val, TypeName)\
+//        val <= static_cast<decltype(val)>(std::numeric_limits<TypeName>::max())\
+//            ?   (void)0 : throw std::overflow_error("overflow in convertion");\
+//        val >= static_cast<decltype(val)>(std::numeric_limits<TypeName>::min())\
+//                ? (void)0 : throw std::underflow_error("underflow in convertion")
+
+
+#undef CONV_RANGE_CHEACK
+#define CONV_RANGE_CHEACK(val, TypeName)\
         val <= static_cast<decltype(val)>(std::numeric_limits<TypeName>::max())\
             ?   (void)0 : throw std::overflow_error("overflow in convertion");\
         val >= static_cast<decltype(val)>(std::numeric_limits<TypeName>::min())\
                 ? (void)0 : throw std::underflow_error("underflow in convertion")
+
+//        if ( !greater_than<TypeName, std::numeric_limits<TypeName>::max()>(val)) {\
+//            throw std::overflow_error("overflow in convertion");\
+//        }\
+//        if ( !less_than<TypeName, std::numeric_limits<TypeName>::min()>(val)) {\
+//            throw std::underflow_error("underflow in convertion");\
+//        }
 
 
 //#undef ERROR_MSG
@@ -58,14 +150,16 @@
 //(__FILE__ "(" + std::to_string((long long int) __LINE__) + "): " + (message)\
 //).c_str()
 //
-//#undef RANGE_CHEACK
-//#define RANGE_CHEACK(val, TypeName)\
+//#undef CONV_RANGE_CHEACK
+//#define CONV_RANGE_CHEACK(val, TypeName)\
 //        val <= static_cast<decltype(val)>(std::numeric_limits<TypeName>::max())\
 //            ?   (void)0 : throw std::overflow_error(ERROR_MSG("overflow in convertion"));\
 //        val >= static_cast<decltype(val)>(std::numeric_limits<TypeName>::min())\
 //                ? (void)0 : throw std::underflow_error(ERROR_MSG("underflow in convertion"))
 
-namespace _detail
+
+
+namespace detail_
 {
  ///////////////////////////
  /// \brief for custom string, modify this.
@@ -80,30 +174,62 @@ namespace _detail
     {
         enum {value = std::is_same<StrType, std::string>::value};
     };
-    uint8_t countDigits(uint64_t val) noexcept {
-        uint8_t digits = 1;
-        while(true) {
-            if (val < 10)   return digits;
-            if (val < 100)  return digits + 1;
-            if (val < 1000) return digits + 2;
-            val /= 1000;
-            digits += 3;
+
+    inline uint32_t countDigits(uint64_t val) noexcept {
+        uint32_t result = 1;
+        for (;;) {
+            if (likely(val < 10)) return result;
+            if (likely(val < 100)) return result + 1;
+            if (likely(val < 1000)) return result + 2;
+            if (likely(val < 10000)) return result + 3;
+        val /= 10000U;
+        result += 4;
         }
     }
-} // namespace _detail
+
+
+
+// max_exponent10 + 20
+
+// char* Print ( const char * format, ... ) {
+
+//     char* ptr = (char*)malloc(sizeof(char) * 58);
+
+
+//     va_list args;
+//     va_start (args, format);
+
+//     vsnprintf(ptr, 58, format, args);
+//     va_end (args);
+//     return ptr;
+// }
+
+//     float db = 123.456;
+//     cout << Print("%f", db);
+
+    inline void getFormatted(char* ptr, int size, const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+        vsnprintf(ptr, size, format, args);
+        va_end(args);
+    }
+} // namespace detail_
 
 //---------------------------------------------------------
 //                                  float append to string
 //----------------------------------------------------------
     template <typename Flo, typename StrType>
-        typename std::enable_if<_detail::isString<StrType>::value
+        inline  typename std::enable_if<detail_::isString<StrType>::value
                                 && std::is_floating_point<Flo>::value,
         void>::type
         appendTo(StrType& str, Flo foo) {
-            std::stringstream sstrm;
-            sstrm << foo;
-            str.append(sstrm.str());
+            auto size = sizeof(char) * (std::numeric_limits<Flo>::max_exponent10 + 20);
+            auto ptr = static_cast<char*>(malloc(size));
+            detail_::getFormatted(ptr, size, "%f", foo);
+            str.append(ptr);
         }
+
+
 //----------------------------------------------------------
 //                                  integer append to string
 //----------------------------------------------------------
@@ -111,13 +237,13 @@ namespace _detail
 //              unsigned integer append to string
 //-----------------------------------------------
     template <typename Int, typename StrType>
-        typename std::enable_if<_detail::isString<StrType>::value
+        inline typename std::enable_if<detail_::isString<StrType>::value
                                 && std::is_integral<Int>::value
                                 && !std::is_signed<Int>::value,
         void>::type
     appendTo(StrType& str, Int val) {
-        if (val) {
-            uint8_t digit = _detail::countDigits((uint64_t)val);
+        if (likely(0 != val)) {
+            uint8_t digit = detail_::countDigits((uint64_t)val);
             char buffer[21];
             buffer[digit] = '\0';
             uint8_t ch = val % 10;
@@ -137,19 +263,19 @@ namespace _detail
 //              signed integer append to string
 //--------------------------------------------
     template <typename Int, typename StrType>
-        typename std::enable_if<_detail::isString<StrType>::value
+        inline typename std::enable_if<detail_::isString<StrType>::value
                                 && std::is_integral<Int>::value
                                 && std::is_signed<Int>::value,
         void>::type
     appendTo(StrType& str, Int val) {
-        if(!val) {
+        if(unlikely( 0 == val)) {
             str.append("0");
             return;
         }
         else if (val < 0) {
             val = -val;
             char buffer[22];
-            uint8_t digit = _detail::countDigits((uint64_t)val);
+            uint8_t digit = detail_::countDigits((uint64_t)val);
             buffer[++digit] = '\0';
             uint8_t ch = val % 10;
             while(val >= 10) {
@@ -162,7 +288,7 @@ namespace _detail
             str.append(buffer);
         }
         else {
-            uint8_t digit = _detail::countDigits((uint64_t)val);
+            uint8_t digit = detail_::countDigits((uint64_t)val);
             char buffer[21];
             buffer[digit] = '\0';
             uint8_t ch = val % 10;
@@ -179,7 +305,7 @@ namespace _detail
 //                    boolean append to std::string
 //-------------------------------------------------
     template<typename StrType>
-        typename std::enable_if<_detail::isString<StrType>::value,void>::type
+        inline  typename std::enable_if<detail_::isString<StrType>::value,void>::type
     appendTo(StrType& acceptor, bool boolean, bool toUpperCase = false) {
         if(toUpperCase) {
             if(boolean)
@@ -198,20 +324,20 @@ namespace _detail
 //                            c-string append to string
 //------------------------------------------------------
     template<typename cCharT, typename StrType>
-        typename std::enable_if<
+        inline typename std::enable_if<
                 std::is_convertible<cCharT,const char*>::value
-                &&_detail::isString<StrType>::value,
+                &&detail_::isString<StrType>::value,
         void>::type
     appendTo( StrType& acceptor, cCharT c_str) {
         auto ptr = static_cast<const char*>(c_str);
-        if(ptr)
+        if(likely(0 != ptr))
             acceptor.append(ptr);
     }
 //-----------------------------------------------------
 //                                char append to string
 //-----------------------------------------------------
     template<typename StrType>
-    typename std::enable_if<_detail::isString<StrType>::value,
+    typename std::enable_if<detail_::isString<StrType>::value,
         void>::type
     appendTo(StrType& str, char ch) {
         char c_str[2];
@@ -223,8 +349,8 @@ namespace _detail
 //                            string append to string
 //----------------------------------------------------
     template <typename StrType1, typename StrType2>
-        typename std::enable_if<_detail::isString<StrType1>::value
-                                && _detail::isString<StrType2>::value,
+        typename std::enable_if<detail_::isString<StrType1>::value
+                                && detail_::isString<StrType2>::value,
         void>::type
     appendTo(StrType1& acceptor, StrType2& src) {
         acceptor.append(src);
@@ -234,16 +360,16 @@ namespace _detail
 //---------------------------------------------------------------------
 //                                                 integrated appending
 //---------------------------------------------------------------------
-template <typename StrType>
-void
-appendTo(StrType& result) {
-}
-template<typename StrType, typename Num, typename... Oth>
-void
-appendTo(StrType& str, Num num, Oth...oth) {
-    appendTo(str, num);
-    appendTo(str, oth...);
-}
+    template <typename StrType>
+    void
+    appendTo(StrType& result) {
+    }
+    template<typename StrType, typename Num, typename... Oth>
+    void
+    appendTo(StrType& str, Num num, Oth...oth) {
+        appendTo(str, num);
+        appendTo(str, oth...);
+    }
 //-----------------------------------------------------------------------
 //                            integers  <<===>>  floating-point numbers
 //-----------------------------------------------------------------------
@@ -253,13 +379,14 @@ appendTo(StrType& str, Num num, Oth...oth) {
             std::is_convertible<SrcType, TargeType>::value,
     TargeType>::type
     to(const SrcType& raws) {
-        RANGE_CHEACK(raws, TargeType);
+        CONV_RANGE_CHEACK(raws, TargeType);
         return static_cast<SrcType>(raws);
     }
-namespace _detail
+
+namespace detail_
 {
     const char*
-    _skipBlanks(const char* c_str) {
+    skipBlanks(const char* c_str) {
         size_t len = strlen(c_str);
         size_t cur = 0;
         for(;cur != len; ++cur) {
@@ -274,8 +401,8 @@ namespace _detail
     }
 
     double
-    _reparse(const char* c_str) {
-        c_str = _skipBlanks(c_str);
+    simple_parse(const char* c_str) {
+        c_str = skipBlanks(c_str);
         size_t len = strlen(c_str);
         switch(c_str[0]) {
         case 'e':   if (len == 1)   return 2.7182818284590452353602874713527;
@@ -295,7 +422,7 @@ namespace _detail
         default : throw std::invalid_argument("unable to parse string to number");
         }// switch
     }
-} // namespace _detail
+} // namespace detail_
 //----------------------------------------------------------------
 //                                           String ==>  number
 //----------------------------------------------------------------
@@ -305,9 +432,11 @@ namespace _detail
     to(const char*);
 
     template <typename Number, typename StrType>
-        typename std::enable_if<_detail::isString<StrType>::value,
+        typename std::enable_if<detail_::isString<StrType>::value,
         Number>::type
     to(StrType&);
+
+
 //////// specification
 #define FUNCTION_FACTORY_STR_TO_NUM(Target, Buff, std_func_call)\
     template<> Target\
@@ -315,10 +444,10 @@ namespace _detail
         Buff buf = std_func_call;\
         if (!buf) {\
             return static_cast<Target>\
-                (_detail::_reparse(c_str));\
+                (detail_::simple_parse(c_str));\
         }\
         else {\
-            RANGE_CHEACK(buf, Target);\
+            CONV_RANGE_CHEACK(buf, Target);\
             return static_cast<Target>(buf);\
         }\
     }\
@@ -331,7 +460,13 @@ namespace _detail
 FUNCTION_FACTORY_STR_TO_NUM(signed short, signed long long int, strtol(c_str, NULL, 0))
 FUNCTION_FACTORY_STR_TO_NUM(unsigned short, unsigned long long int, strtoul(c_str, NULL, 0))
 FUNCTION_FACTORY_STR_TO_NUM(signed int,  signed long long int, strtol(c_str, NULL, 0))
-FUNCTION_FACTORY_STR_TO_NUM(unsigned int, unsigned long long int, strtoll(c_str, NULL, 0))
+FUNCTION_FACTORY_STR_TO_NUM(unsigned int, unsigned long long int, strtoul(c_str, NULL, 0))
+
+
+FUNCTION_FACTORY_STR_TO_NUM(signed long  int,  signed long long int, strtoll(c_str, NULL, 0))
+FUNCTION_FACTORY_STR_TO_NUM(unsigned long int,  unsigned long long int, strtoull(c_str, NULL, 0))
+
+
 FUNCTION_FACTORY_STR_TO_NUM(signed long long int,  signed long long int, strtoll(c_str, NULL, 0))
 FUNCTION_FACTORY_STR_TO_NUM(unsigned long long int, unsigned long long int, strtoull(c_str, NULL, 0))
 
@@ -345,7 +480,7 @@ FUNCTION_FACTORY_STR_TO_NUM(long double, long double, strtold(c_str, NULL))
 //----------------------------------------------------------------
     template<> bool
     to<bool>(const char* c_str) {
-        c_str = _detail::_skipBlanks(c_str);
+        c_str = detail_::skipBlanks(c_str);
         size_t len = strlen(c_str);
         switch(c_str[0]) {
         case '0':   if (len == 1)   return false;
@@ -388,7 +523,7 @@ FUNCTION_FACTORY_STR_TO_NUM(long double, long double, strtold(c_str, NULL))
 //                                          multi type args  ==>  string
 //----------------------------------------------------------------------
     template<typename StrType, typename... Raws>
-        typename std::enable_if<_detail::isString<StrType>::value,
+        typename std::enable_if<detail_::isString<StrType>::value,
         StrType>::type
     to(const Raws&...rawMaterial) {
         StrType str;
